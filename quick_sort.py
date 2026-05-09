fdd9a61998dfcbc7cff9c01b9c37b66229d0f634
HiDMadMad <madmadpv@gmail.com>
2026-05-09 17:18:32 +0330

    buggy one..
    i like it but slicing creates a copy of the array...  (i wanted to git-stash it but i can not=) )

---
+++ quick_sort.py
@@ -0,0 +1,31 @@
+random_numbers = [6, 3, 5, 8, 4, 2, 9, 1, 5]
+print(random_numbers)
+
+def quick(array:list):
+    pivot = array[len(array)-1]  # a random item:  first=array[0], last=array[len(array)-1], mid=array[len(array)//2], random=random.choice(array)
+    green=0  # i
+    orange=-1  # j
+
+    while(green<len(array)-1):
+        if(array[green]<pivot):
+            orange+=1
+            array[orange], array[green] = array[green], array[orange]
+        green+=1
+    orange+=1
+    array[orange], array[len(array)-1] = array[len(array)-1], array[orange]
+    # orange is pivot index now
+
+    if(len(array[:orange])>1):
+        print(f"[:o] {array[:orange]}")
+        quick(array[:orange])
+
+    if(len(array[orange+1:])>1):
+        print(f"[o+1:] {array[orange+1:]}")
+        quick(array[orange+1:])
+
+
+
+
+quick(random_numbers)
+print(random_numbers)
+# print(quick(random_numbers))
